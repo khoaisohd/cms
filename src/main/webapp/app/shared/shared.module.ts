5da16c4bd24a87b7420b4cf89d4f80a275a3623c
tom_hoang <hoangdinhphuong89@gmail.com>
2017-06-06 20:36:18 -0500

    Initial Commit

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,56 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { CookieService } from 'angular2-cookie/services/cookies.service';
+import {
+    CmsSharedLibsModule,
+    CmsSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    Principal,
+    HasAnyAuthorityDirective,
+    JhiSocialComponent,
+    SocialService,
+    JhiLoginModalComponent
+} from './';
+
+@NgModule({
+    imports: [
+        CmsSharedLibsModule,
+        CmsSharedCommonModule
+    ],
+    declarations: [
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        CookieService,
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        AuthServerProvider,
+        SocialService,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        CmsSharedCommonModule,
+        JhiSocialComponent,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class CmsSharedModule {}

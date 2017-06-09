09771377e140e84bbf1d5dca761a42f2c97107f9
tom_hoang <hoangdinhphuong89@gmail.com>
2017-06-09 11:14:08 -0500

    Register course

--- src/main/webapp/app/entities/course/course-catalog.component.ts
+++ src/main/webapp/app/entities/course/course-catalog.component.ts
@@ -4,6 +4,7 @@ import { ResponseWrapper, Principal } from '../../shared';
 
 import { Course } from './course.model';
 import { CourseService } from './course.service';
+import { Student } from '../student';
 
 @Component({
     selector: 'course-catalog',
@@ -46,6 +47,19 @@ export class CourseCatalogComponent implements OnInit, OnDestroy {
         if (this.currentAccount) {
             return course.students.map(student => student.id).indexOf(this.currentAccount.id) > -1;
         }
+    }
+
+    enroll(course: Course): void {
+        const student: Student = {
+            id: this.currentAccount.id
+        };
+        course.students.push(student);
+        this.courseService.update(course).subscribe(
+            (res: ResponseWrapper) => {
+                console.log(res);
+            },
+            (res: ResponseWrapper) => console.log(res)
+        );
     }
 
     private onError(error) {
